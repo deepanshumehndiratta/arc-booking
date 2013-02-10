1051b89df7eb3d0ece216538f6e7aee9e626b80e
Deepanshu Mehndiratta <deepanshumehndiratta@gmail.com>
2013-02-10 19:05:24 +0530

    Application Setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArcBooking::Application.config.secret_token = '9ff1c9e860eb51d950c0a4c5ff8cdd9559c353b5f242e9b55c4a1037532e983065cd42112f91f7eb2f0bf726915ba35926f84c65033dd03fa92881dacc3eda9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ArcBooking::Application.config.session_store :cookie_store, key: '_arc-booking_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ArcBooking::Application.config.session_store :active_record_store
